0ddeaf4f002a91a1d7b2c892f5df5f1629fe7575
Ishan1005 <ig20april@gmail.com>
2024-10-15 17:55:41 +0400

    OOps

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-class IOS():
-    def __init__(self):
-        self.str1=""
-    def get_value(self):
-        self.str1 = input("Enter a word(NOT CAPITAL)")
-    def print_value(self):
-        print("The result is", self.str1.upper())
-obj = IOS()
-obj.get_value()
-obj.print_value()
+class vehicle:
+    def __init__(self,name):
+      self.name = name 
+obj = vehicle("bugati")
+print("car name is ",obj.name)
